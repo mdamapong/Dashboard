89b59b30829beee0103f6bd5c10e56b29d06b063
mickey <damapongm@gmail.com>
2023-02-17 15:52:16 -0800

    total rev

--- client/src/components/charts/TotalRevenue.tsx
+++ client/src/components/charts/TotalRevenue.tsx
@@ -1,6 +1,7 @@
 import ReactApexChart from 'react-apexcharts';
-import { Box, Typography, Stack, display } from '@pankod/refine-mui';
+import { Box, Typography, Stack } from '@pankod/refine-mui';
 import { ArrowCircleUpRounded } from '@mui/icons-material';
+
 import { TotalRevenueOptions, TotalRevenueSeries } from './chart.config';
 
 const TotalRevenue = () => {
@@ -14,8 +15,32 @@ const TotalRevenue = () => {
 			flexDirection='column'
 			borderRadius='15px'>
 			<Typography fontSize={18} fontWeight={600} color='#11142d'>
-				Total Revenuesssdd
+				Total Revenue
 			</Typography>
+
+			<Stack my='20px' direction='row' gap={4} flexWrap='wrap'>
+				<Typography fontSize={28} fontWeight={700} color='#11142d'>
+					$236,535
+				</Typography>
+				<Stack direction='row' alignItems='center' gap={1}>
+					<ArrowCircleUpRounded sx={{ fontSize: 25, color: '#475be8' }} />
+					<Stack>
+						<Typography fontSize={15} color='#475be8'>
+							0.8%
+						</Typography>
+						<Typography fontSize={12} color='#808191'>
+							Than Last Month
+						</Typography>
+					</Stack>
+				</Stack>
+			</Stack>
+
+			<ReactApexChart
+				series={TotalRevenueSeries}
+				type='bar'
+				height={310}
+				options={TotalRevenueOptions}
+			/>
 		</Box>
 	);
 };
